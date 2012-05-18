bc9b8357ba752ace4e330b49cf8abd2203f53594
nefarius <nefarius@dhmx.at>
2012-05-18 23:32:06 +0200

    Swapped interactive mode with command line switches Added Debug configuration

--- Injector/Main.cpp
+++ Injector/Main.cpp
@@ -16,7 +16,7 @@
 #include <algorithm>
 
 // Entry point
-int main()
+int _tmain(int argc, TCHAR** argv)
 {
 	try
 	{
@@ -42,98 +42,103 @@ int main()
 #endif
 		std::tcout << _T("Copyright (c) 2009 Cypher. All rights reserved.") << std::endl << std::endl;
 
-		// Prompt for input
-		std::tcout << _T("Please choose a method to choose the target process:") << std::endl;
-		std::tcout << _T("1. Process Name") << std::endl;
-		std::tcout << _T("2. Window Name") << std::endl;
-		std::tcout << _T("3. Process ID") << std::endl;
-		std::tcout << _T("Choice: ") << std::endl;
-
-		// Get method
-		FindProcMethod InjectType;
-		std::tcin >> InjectType;
-		if (!std::tcin.good())
-			throw new std::out_of_range("Invalid input for search method.");
+		static struct option long_options[] =
+		{
+			{ _T("process-name"),	ARG_REQ,	0, FindProcName },
+			{ _T("window-name"),	ARG_REQ,	0, FindProcWindow },
+			{ _T("process-id"),		ARG_REQ,	0, FindProcID },
+			{ _T("module-name"),	ARG_REQ,	0, 0 },
+			{ _T("inject"),			ARG_NONE,	0, 4 },
+			{ _T("eject"),			ARG_NONE,	0, 5 }
+		};
 
 		// Variable to store process ID
-		DWORD ProcID;
+		DWORD ProcID = 0;
 		// Fully qualified module path
-		std::tstring ModulePath(Injector::Get()->GetPath(ModuleName));
+		std::tstring ModulePath;
 
-		// Switch over method
-		switch (InjectType)
+		int option_index = 0, c;
+
+		do 
 		{
-			// Find and inject via process name
-		case FindProcName:
+			c = getopt_long(argc, argv, _T(""), long_options, &option_index);
+
+			// Switch over method
+			switch (c)
 			{
-				// Prompt for input
-				std::tcout << _T("Please enter the process name: ") << std::endl;
-
-				// Get entered process name and convert to lowercase
-				std::tstring ProcessName;
-				std::tcin.sync();
-				std::getline(std::tcin,ProcessName);
-				std::transform(ProcessName.begin(),ProcessName.end(),ProcessName.begin(),tolower);
-
-				// Attempt injection via process name
-				ProcID = Injector::Get()->GetProcessIdByName(ProcessName);
-
+			case 0:
+				ModulePath = Injector::Get()->GetPath(optarg);
 				break;
+				// Find and inject via process name
+			case FindProcName:
+				{
+					// Get entered process name and convert to lowercase
+					std::tstring ProcessName(optarg);
+					std::transform(ProcessName.begin(),ProcessName.end(),ProcessName.begin(),tolower);
+
+					std::tcout << _T("Process name given: ") << ProcessName << std::endl;
+
+					// Attempt injection via process name
+					ProcID = Injector::Get()->GetProcessIdByName(ProcessName);
+					std::tcout << _T("Process identifier: ") << ProcID << std::endl;
+
+					break;
+				}
+				// Find and inject via window name
+			case FindProcWindow:
+				{
+					// Get entered window name
+					std::tstring WindowName(optarg);
+
+					std::tcout << _T("Window name given: ") << WindowName << std::endl;
+
+					// Attempt injection via window name
+					ProcID = Injector::Get()->GetProcessIdByWindow(WindowName);
+
+					break;
+				}
+				// Find and inject via process id
+			case FindProcID:
+				{
+					std::tstring ProcessID(optarg);
+
+					std::tcout << _T("Process identifier given: ") << ProcessID << std::endl;
+
+					// Check if entered PID was valid
+					ProcID = _tstoi(ProcessID.c_str());
+
+					break;
+				}
 			}
-			// Find and inject via window name
-		case FindProcWindow:
-			{
-				// Prompt for input
-				std::tcout << _T("Please enter the window name: ") << std::endl;
-
-				// Get entered window name
-				std::tstring WindowName;
-				std::tcin.sync();
-				std::getline(std::tcin,WindowName);
-
-				// Attempt injection via window name
-				ProcID = Injector::Get()->GetProcessIdByWindow(WindowName);
-			
-				break;
-			}
-			// Find and inject via process id
-		case FindProcID:
-			{
-				// Prompt for input
-				std::tcout << _T("Please enter the process identifier: ") << std::endl;
-
-				// Get entered process id
-				std::tcin.sync();
-				std::tcin >> ProcID;
-
-				// Check if entered PID was valid
-				if (!std::tcin.good())
-					throw std::out_of_range("Invalid input for process id.");
-
-				break;
-			}
-			// Unknown method
-		default:
-			throw new std::out_of_range("Invalid value for process finding method.");
-		}
+		} while (c != -1);
 
 		// Get privileges required to perform the injection
 		Injector::Get()->GetSeDebugPrivilege();
-		// Inject module
-		Injector::Get()->InjectLib(ProcID,ModulePath);
-		// If we get to this point then no exceptions have been thrown so we
-		// assume success.
-		std::tcout << "Successfully injected module!" << std::endl;
-		// Stop automatic ejection
-		std::tcin.clear();
-		std::tcin.sync();
-		std::tcin.get();
-		// Eject module
-		Injector::Get()->EjectLib(ProcID,ModulePath);
-		// If we get to this point then no exceptions have been thrown so we
-		// assume success.
-		std::tcout << "Successfully ejected module!" << std::endl;
 
+		optind = 1;
+
+		do 
+		{
+			c = getopt_long(argc, argv, _T(""), long_options, &option_index);
+
+			switch(c)
+			{
+			case 4:
+				// Inject module
+				Injector::Get()->InjectLib(ProcID,ModulePath);
+				// If we get to this point then no exceptions have been thrown so we
+				// assume success.
+				std::tcout << "Successfully injected module!" << std::endl;
+				break;
+			case 5:
+				// Eject module
+				Injector::Get()->EjectLib(ProcID,ModulePath);
+				// If we get to this point then no exceptions have been thrown so we
+				// assume success.
+				std::tcout << "Successfully ejected module!" << std::endl;
+				break;
+			}
+		} while (c != -1);
 	}
 	// Catch STL-based exceptions.
 	catch (const std::exception& e)
@@ -161,11 +166,6 @@ int main()
 		std::tcout << "Unknown error!" << std::endl;
 	}
 
-	// Stop window from automatically closing
-	std::tcin.clear();
-	std::tcin.sync();
-	std::tcin.get();
-
 	// Return success
-	return 0;
+	return ERROR_SUCCESS;
 }
