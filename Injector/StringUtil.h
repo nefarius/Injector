ff984a1dd49ae1518340be026c3da3b2a12d661f
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-10-12 15:33:01 +0200

    Fixed some build warnings

--- Injector/StringUtil.h
+++ Injector/StringUtil.h
@@ -4,26 +4,29 @@
 #include <locale>
 #include <algorithm>
 #include <iterator>
-#include <codecvt>
+
 
 template <typename S_type>
 S_type toLower(const S_type& in)
 {
-    S_type out;
-    std::transform(in.begin(), in.end(), std::back_inserter(out), [](S_type::value_type ch) {
-        return std::tolower(ch, std::locale()); });
-    return out;
+	S_type out;
+	std::transform(in.begin(), in.end(), std::back_inserter(out), [](typename S_type::value_type ch)
+	{
+		return std::tolower(ch, std::locale());
+	});
+	return out;
 }
 
-inline std::wstring utf8_to_wstr(const std::string& utf8) {
-    const auto wideCharCount = MultiByteToWideChar(CP_UTF8, 0, utf8.data(), utf8.size(), nullptr, 0);
-    std::wstring wstr;
-    wstr.resize(wideCharCount);
+inline std::wstring utf8_to_wstr(const std::string& utf8)
+{
+	const auto wideCharCount = MultiByteToWideChar(CP_UTF8, 0, utf8.data(), static_cast<int>(utf8.size()), nullptr, 0);
+	std::wstring wstr;
+	wstr.resize(wideCharCount);
 
-    // While the docs say the return value includes trailing null, this is only the case if the
-    // input has a trailing null, and if we include it in the size parameter.
-    //
-    // utf8.size() excludes the trailing null, so the wstring does too
-    MultiByteToWideChar(CP_UTF8, 0, utf8.data(), utf8.size(), wstr.data(), wideCharCount);
-    return wstr;
+	// While the docs say the return value includes trailing null, this is only the case if the
+	// input has a trailing null, and if we include it in the size parameter.
+	//
+	// utf8.size() excludes the trailing null, so the wstring does too
+	MultiByteToWideChar(CP_UTF8, 0, utf8.data(), static_cast<int>(utf8.size()), wstr.data(), wideCharCount);
+	return wstr;
 }
